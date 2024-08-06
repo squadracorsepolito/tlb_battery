e0c3c7c5eb577147d9cf96ebcc48d3148423c74a
Simone Ruffini <simone.ruffini.work@gmail.com>
2024-08-06 11:21:13 +0200

    fix: fw version in conf

--- Core/Inc/conf.h
+++ Core/Inc/conf.h
@@ -23,8 +23,8 @@
 
 /* ---------- Exported constants --------------------------------------------*/
 #define VERSION_MAJOR 3
-#define VERSION_MINOR 1
-#define VERSION_PATCH 11
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
 
 #define DEBUG_SERIAL
 #define DEBUG_AIN
